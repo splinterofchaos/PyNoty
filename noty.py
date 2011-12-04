b13883cd416f995a4793b632f8200e1d780f324b
Splinter of Chaos <hakusa@gmail.com>
2011-12-04 02:53:39 -0500

    Window funtionally works.

--- noty.py
+++ noty.py
@@ -7,13 +7,29 @@ size = width, height
 BLACK = 0, 0, 0
 WHITE = 255, 255, 255
 
+class Window:
+    def __init__(self, x, y ):
+        pygame.init()
+
+        self.dimensions = x, y
+        self.screen = pygame.display.set_mode( self.dimensions )
+
+        self.font = pygame.font.Font( None, 20 )
+
+        self.clearColor = 0, 0, 0
+
+    def paint( self, obj, pos ):
+        self.screen.blit( obj, pos )
+
+    def display( self ):
+        pygame.display.flip()
+
+        self.screen.fill( self.clearColor )
 
 if __name__ == '__main__':
-    pygame.init()
-    screen = pygame.display.set_mode( size )
+    window = Window( 500, 500 )
 
-    font = pygame.font.Font( None, 25 )
-    text = font.render( "Hello World", True, WHITE )
+    text = window.font.render( "Hello World", True, WHITE )
 
     keepGoing = True
     while keepGoing:
@@ -24,9 +40,7 @@ if __name__ == '__main__':
                 if e.key == pygame.K_ESCAPE:
                     keepGoing = False
 
-        screen.blit( text, [50,50] )
-
-        pygame.display.flip()
-        screen.fill( BLACK )
+        window.paint( text, [50,50] )
+        window.display()
 
     pygame.quit()
