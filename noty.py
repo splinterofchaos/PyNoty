43094efec2ab02d3bb37319b5e2e1a4644c30281
Splinter of Chaos <hakusa@gmail.com>
2011-12-04 02:18:57 -0500

    Pygame window created and destroyed.

--- noty.py
+++ noty.py
@@ -1,3 +1,16 @@
 
+import sys, pygame
+
+width = 100
+height = width
+size = width, height
+BLACK = 0, 0, 0
+
+
 if __name__ == '__main__':
-    print "Hello World"
+    pygame.init()
+
+    screen = pygame.display.set_mode( size )
+
+    screen.fill( BLACK )
+    pygame.display.flip()
