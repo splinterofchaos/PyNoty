f745f828921155c06c8a7c34d6aa2e6abb6fcb92
Splinter of Chaos <hakusa@gmail.com>
2011-12-04 03:06:23 -0500

    Renderable natively accepts a string.
    It takes the string and converts it to a graphical object.

--- noty.py
+++ noty.py
@@ -1,25 +1,19 @@
 
 import sys, pygame
 
+pygame.init()
+
 width = 500
 height = width
 size = width, height
 BLACK = 0, 0, 0
 WHITE = 255, 255, 255
 
-class Renderable:
-    def __init__( self, obj, pos ):
-        self.obj = obj
-        self.pos = pos
-
 class Window:
     def __init__(self, x, y ):
-        pygame.init()
-
         self.dimensions = x, y
         self.screen = pygame.display.set_mode( self.dimensions )
 
-        self.font = pygame.font.Font( None, 20 )
 
         self.clearColor = 0, 0, 0
 
@@ -31,11 +25,21 @@ class Window:
 
         self.screen.fill( self.clearColor )
 
+
+class Renderable:
+    font = pygame.font.Font( None, 20 )
+
+    def __init__( self, obj, pos, color=WHITE ):
+        if isinstance( obj, str ):
+            obj = Renderable.font.render( obj, True, color )
+
+        self.obj = obj
+        self.pos = pos
+
 if __name__ == '__main__':
     window = Window( 500, 500 )
 
-    textTmp = window.font.render( "Hello World", True, WHITE )
-    hello = Renderable( textTmp, [50,50] )
+    hello = Renderable( "Hello World", [50,50] )
 
     keepGoing = True
     while keepGoing:
