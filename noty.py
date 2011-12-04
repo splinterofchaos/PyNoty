31104a1834df11cdbbc5c76ee0a1ecdda0187b8e
Splinter of Chaos <hakusa@gmail.com>
2011-12-04 02:23:17 -0500

    Window waits until closed via X.

--- noty.py
+++ noty.py
@@ -9,8 +9,15 @@ BLACK = 0, 0, 0
 
 if __name__ == '__main__':
     pygame.init()
-
     screen = pygame.display.set_mode( size )
 
-    screen.fill( BLACK )
-    pygame.display.flip()
+    keepGoing = True
+    while keepGoing:
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                keepGoing = False
+
+        screen.fill( BLACK )
+        pygame.display.flip()
+
+    pygame.quit()
