3edd621b786b03d74bb220e4d926943f7f510c7a
Splinter of Chaos <hakusa@gmail.com>
2011-12-05 07:17:27 -0500

    Vectors can be added.

--- vector.py
+++ vector.py
@@ -3,6 +3,7 @@ class Vector:
     def __init__( self, vec ):
         self.data = vec
 
+    # Python functions.
     def __getitem__( self, key ):
         return self.data[ key ]
     def __setitem__( self, key, value ):
@@ -21,10 +22,33 @@ class Vector:
         
         return string + ' >'
 
+    def __add__( self, other ):
+        try:
+            if len(self) < len(other):
+                minVec = self
+                maxVec = other
+            else:
+                minVec = other
+                maxVec = self
+
+            ret = Vector( [] )
+
+            for i in range( len(minVec) ):
+                ret.data.append( minVec[i] + maxVec[i] )
+            for i in range( len(minVec), len(maxVec) ):
+                ret.data.append( maxVec[i] )
+
+            return ret
+        except TypeError:
+            raise TypeError
+                
+
 if __name__ == '__main__':
     a = Vector( [1,2] )
+    b = Vector( [2,1] )
     a[0] = 5
-    b = a[1]
     print 'a      =', a
+    print 'b      =', b
     print 'len(a) =', len( a )
-    print 'b[1]   =', b
+    print 'a[1]   =', a[1]
+    print 'a + b  =', a + b
